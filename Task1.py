6963fe10d50e9bce223da61e20df57a783e305d8
Vasiliy <vasiliyasazonov@gmail.com>
2022-10-30 13:43:33 +0300

    complete task1

--- Task1.py
+++ Task1.py
@@ -11,13 +11,41 @@ def genarate_rand_num(number_quantity):
 
     array_rand_num = []
 
-    for i in number_quantity:
+    for i in range(1, number_quantity+1):
 
         rand_num = randrange(1, 10)
         array_rand_num.append(rand_num)
 
-    print(array_rand_num)
+    return(array_rand_num)
+
+def create_array_odd(array_full):
+
+    array_odd = []
+    count = 0
+
+    for i in array_full:
+        if (count % 2 != 0):
+            array_odd.append(i)
+            count += 1
+        else:
+            count +=1
+            continue
+
+    return array_odd
+
+def sum_array(array):
+
+    sum_array = 0
+
+    for i in array:
+        sum_array += i
+    
+    return sum_array
 
 
-    number_quantity = input('input: ')
-    genarate_rand_num(number_quantity)
+number_quantity = int(input('input quantity numbers: '))
+array = genarate_rand_num(number_quantity)
+print(array)
+array_odd = create_array_odd(array)
+sum_odd_num = sum_array(array_odd)
+print(f'на нечетных позициях {array_odd}, ответ: {sum_odd_num} ')
